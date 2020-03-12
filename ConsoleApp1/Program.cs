f2f7006b171a338e259e652bb29b1e014bc3f0e5
howard0303 <howard010203@gmail.com>
2020-03-12 20:17:26 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("購買金額");
+            int money = 1000;
+
+            while (true)
+            {
+                //判斷
+                if (money <= 0)
+                {
+                    Console.WriteLine("你已經破產...");
+                    break;
+                }
+
+                //檢查花費
+              
+               
+                string costString = Console.ReadLine();
+                int cost = int.Parse(costString);
+               
+
+                //扣錢
+
+                money = money - cost;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
